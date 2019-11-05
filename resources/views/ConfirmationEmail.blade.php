cc6a51ad5e8b82effaf7dd03b834fe0aa1f842fb
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2019-11-05 08:14:12 +0700

    update email

--- resources/views/ConfirmationEmail.blade.php
+++ resources/views/ConfirmationEmail.blade.php
@@ -1,11 +1,17 @@
 @component('mail::message')
-# Hi! {{ $name }}
+# Dear Pejuang NPLC, Kelompok {{ $name }}
 
 {{ $mail['message']  }}
 
-@component('mail::button', ['url' => 'https://fusionsvisual.com'])
-Confirm
+@component('mail::button', ['url' => 'https://forms.gle/R2jEpUF17fJghtuz6'])
+Form Daftar Ulang
 @endcomponent
+
+# Pendaftaran ulang paling lambat:
+<p><span style="padding-right: 17px">Hari</span>     :  <b>Rabu, 6 November 2019</b></p>
+<p><span>Waktu</span>    : <b>23.59 WIB</b></p>
+
+Setelah waktu diatas, peserta akan didiskualifikasi dari babak final 7th NPLC secara otomatis.
 
 Regards,<br>
 7th NPLC Team.
